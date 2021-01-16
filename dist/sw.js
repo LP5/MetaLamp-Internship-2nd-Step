430847ca55d0b961784f425382bb2a497f274a04
LP5 <noobdoomguy8658@gmail.com>
2021-01-16 18:53:53 +0500

    Начальная настройка шрифтов

--- dist/sw.js
+++ dist/sw.js
@@ -108,8 +108,20 @@ define("./sw.js",['./workbox-c49a797c'], function (workbox) { 'use strict';
    */
 
   workbox.precacheAndRoute([{
+    "url": "/fonts/Montserrat-Regular.eot",
+    "revision": "af0492c24e83e57b6b8a843d96c818af"
+  }, {
+    "url": "/fonts/Montserrat-Regular.svg",
+    "revision": "7066b320b56bc49a45b07c5f23d9b9e4"
+  }, {
+    "url": "/fonts/Montserrat-Regular.ttf",
+    "revision": "ee6539921d713482b8ccd4d0d23961bb"
+  }, {
+    "url": "/fonts/Montserrat-Regular.woff",
+    "revision": "c5b3f5f9512bd455789cb18694940241"
+  }, {
     "url": "main.js",
-    "revision": "bad153522a34817d602e9a3879feb3f9"
+    "revision": "4fec7565fae0f657ab4d88c65d2c8135"
   }], {});
 
 });
