b6cfdcaa216a7cf159fffe5836106d2c29c9e00e
LP5 <noobdoomguy8658@gmail.com>
2021-01-18 12:55:12 +0500

    ББлок макета с цветом и заголовками теперь расположен горизонтально

--- dist/sw.js
+++ dist/sw.js
@@ -109,7 +109,7 @@ define("./sw.js",['./workbox-c49a797c'], function (workbox) { 'use strict';
 
   workbox.precacheAndRoute([{
     "url": "main.js",
-    "revision": "3f14679e68fe1c710005eebc43ecf336"
+    "revision": "d5c82b2521c067874bf1cebc58b97593"
   }], {});
 
 });
