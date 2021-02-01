09d69052a0abfc2f4ee3e579394ea2547fe491c6
lp5 <noobdoomguy8658@gmail.com>
2021-02-02 04:41:30 +0500

    Немного измены размеры форм элементов, удалены неиспользуемые стили, созданы правила подписаи состояния элементов

--- dist/sw.js
+++ dist/sw.js
@@ -121,7 +121,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "1a15a310fedee7d941500d48c2934470"
+    "revision": "543da2ff307023649c78d23c3627945d"
   }], {});
 
 });
