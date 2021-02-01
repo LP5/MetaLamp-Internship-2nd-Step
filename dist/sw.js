88770c6e564f63071e3f34cba919e4aca6fd442a
LP5 <noobdoomguy8658@gmail.com>
2021-02-01 13:51:27 +0500

    Выставлено расположение элементов Dropdown Expanded

--- dist/sw.js
+++ dist/sw.js
@@ -121,7 +121,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "db7d016678ffe2135e6b5a95ea64bf8a"
+    "revision": "bf2e97b10496510917a7d15ab3f5687a"
   }], {});
 
 });
