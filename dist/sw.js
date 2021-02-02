2068089e6ddc5d73c727cabb208e6f0c5d5a5e84
LP5 <noobdoomguy8658@gmail.com>
2021-02-03 02:50:52 +0500

    Добавлены некоторые элементы Checkbox Buttons, изменения в файлах стилей

--- dist/sw.js
+++ dist/sw.js
@@ -121,7 +121,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "4a24541c335dc1e2d94b25b6b5389788"
+    "revision": "b1ef82734ef7f6aa7cd68ecc16a3d484"
   }], {});
 
 });
