f66c41f5d2e7a0ba977a99a6faf5b3eaf1b7ab1f
LP5 <noobdoomguy8658@gmail.com>
2021-02-02 14:17:31 +0500

    Добавлены тень и граница Dropdown Expanded

--- dist/sw.js
+++ dist/sw.js
@@ -121,7 +121,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "5b0801dde4d57a299c8d938587ed3c23"
+    "revision": "4a24541c335dc1e2d94b25b6b5389788"
   }], {});
 
 });
