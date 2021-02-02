6b7f3496079400a6a4bf74b4001c4b09d5c84ac1
LP5 <noobdoomguy8658@gmail.com>
2021-02-03 03:08:47 +0500

    Настроены миксины текста

--- dist/sw.js
+++ dist/sw.js
@@ -121,7 +121,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "b1ef82734ef7f6aa7cd68ecc16a3d484"
+    "revision": "b5ffc08f2dd406d8fc10bff8a6830fca"
   }], {});
 
 });
