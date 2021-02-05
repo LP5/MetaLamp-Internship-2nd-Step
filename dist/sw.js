c811e608a1bc66b5929e953a67fe6dc01532c834
lp5 <noobdoomguy8658@gmail.com>
2021-02-06 03:59:17 +0500

    Переделан черновик радиокнопок (кнопки сделаны через span), радиокнопка теперь имеет зазор между кружком и границей

--- dist/sw.js
+++ dist/sw.js
@@ -121,7 +121,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "f44518c97044e5ff9dbf25894fc3bae2"
+    "revision": "7f403982b9a94b8bf48a800e41a93b11"
   }], {});
 
 });
