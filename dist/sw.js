d291485fb70d560f3a7db43aa1be8be492cca4b4
lp5 <noobdoomguy8658@gmail.com>
2021-02-06 04:04:41 +0500

    Градиент радиокнопок теперь заливает не всю кпонку, как раньше

--- dist/sw.js
+++ dist/sw.js
@@ -121,7 +121,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "7f403982b9a94b8bf48a800e41a93b11"
+    "revision": "ec9f0db3902cdddd3a5987816f9db4e3"
   }], {});
 
 });
