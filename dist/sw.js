0716d036ccf70e058a7d9a152fce1af0dbff99cc
LP5 <noobdoomguy8658@gmail.com>
2021-02-08 12:51:55 +0500

    Радиокнопки получили соответствующую макету стилизацию (цвет границы и круга внутри)

--- dist/sw.js
+++ dist/sw.js
@@ -121,7 +121,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "221724b633f382e8be1919ee4d39946b"
+    "revision": "bcd1e66d2578c272f950410a2ae4a2f5"
   }], {});
 
 });
