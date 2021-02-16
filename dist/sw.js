c6388a64e9d692390951561e15957e78327093c9
LP5 <noobdoomguy8658@gmail.com>
2021-02-16 18:31:51 +0500

    Добавлены шаблоны Rate Buttons

--- dist/sw.js
+++ dist/sw.js
@@ -133,7 +133,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "7a1ab8a66a432a466028493fc3a17456"
+    "revision": "ff6c98c08ffe88fbc52f809e7ab97c2c"
   }], {});
 
 });
