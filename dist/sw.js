21da940a9028db6ec27edec00cae5e06c66099ae
LP5 <noobdoomguy8658@gmail.com>
2021-02-16 18:50:38 +0500

    Добавлена иконка стрелки к широкой кнопке

--- dist/sw.js
+++ dist/sw.js
@@ -133,7 +133,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "ff6c98c08ffe88fbc52f809e7ab97c2c"
+    "revision": "e3c55bded68f30210d97161b3a313b43"
   }], {});
 
 });
