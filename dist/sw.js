542f63ec3204e241dbcbb5f9e7e14ad93ce08968
LP5 <noobdoomguy8658@gmail.com>
2021-02-17 18:29:34 +0500

    Исправлены ошибки в разметке и стилизации Rate Button

--- dist/sw.js
+++ dist/sw.js
@@ -133,7 +133,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "cd3f74bff7e6a6939eedac66d2ed32d2"
+    "revision": "d97a7b655e3c6fc08ff1e5131fda765b"
   }], {});
 
 });
