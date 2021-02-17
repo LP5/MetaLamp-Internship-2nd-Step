d9638729daa3f127dd16c881536bb4f81e676cf4
LP5 <noobdoomguy8658@gmail.com>
2021-02-17 18:36:29 +0500

    Установлен padding: 0; для Rate Button для лучшего отображения и соответствия макету

--- dist/sw.js
+++ dist/sw.js
@@ -133,7 +133,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "fd7ced686595d563d0fae01114fb34c2"
+    "revision": "4c0aa7122cb9d55308baa924deb5f852"
   }], {});
 
 });
