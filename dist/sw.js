ea7e50c7c96fe68f2c6d2109c927a7326a3bd13f
LP5 <noobdoomguy8658@gmail.com>
2021-02-17 18:33:01 +0500

    Изменены названия классов Rate Button

--- dist/sw.js
+++ dist/sw.js
@@ -133,7 +133,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "c5b3f5f9512bd455789cb18694940241"
   }, {
     "url": "main.js",
-    "revision": "d97a7b655e3c6fc08ff1e5131fda765b"
+    "revision": "fd7ced686595d563d0fae01114fb34c2"
   }], {});
 
 });
