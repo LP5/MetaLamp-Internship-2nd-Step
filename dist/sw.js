9c7426ead1c052da9a11f4d9a493424efa8ac9e1
lp5 <noobdoomguy8658@gmail.com>
2021-02-22 16:50:03 +0500

    Отступы Сведений о номере приведены в большее соответствие макету

--- dist/sw.js
+++ dist/sw.js
@@ -139,7 +139,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "e74a04049ac910f239e01882246b74fc"
   }, {
     "url": "main.js",
-    "revision": "0857e92209ca0df48c8902b481fe198b"
+    "revision": "5f7c95ee17129db952cb6e8a81b051b2"
   }], {});
 
 });
