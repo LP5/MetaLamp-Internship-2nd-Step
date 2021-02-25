640e099a3db9f87781ce5d2f68e31a767d4a2ae0
LP5 <noobdoomguy8658@gmail.com>
2021-02-25 11:21:22 +0500

    Исправлена ошибка в отображении Rich Checkbox Buttons

--- dist/sw.js
+++ dist/sw.js
@@ -139,7 +139,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "e74a04049ac910f239e01882246b74fc"
   }, {
     "url": "main.js",
-    "revision": "33fed4397a82a6529f1da71b182c63be"
+    "revision": "d418f65d79cb9826b5053ffa56c15eae"
   }], {});
 
 });
