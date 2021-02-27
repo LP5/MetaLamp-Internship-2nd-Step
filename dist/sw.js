34d644989fd9b5bc6218f9f2c04a83c3b492017c
lp5 <noobdoomguy8658@gmail.com>
2021-02-27 12:54:34 +0500

    Добавлен аттрибут и псевдокласс disabled для Buttons

--- dist/sw.js
+++ dist/sw.js
@@ -139,7 +139,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "e74a04049ac910f239e01882246b74fc"
   }, {
     "url": "main.js",
-    "revision": "ca0631afc3615136350a378fb2eafcb8"
+    "revision": "1e7180c92cdb47347edc63bc7a9ca224"
   }], {});
 
 });
