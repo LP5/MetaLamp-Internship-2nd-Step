6f6678b544328c1b9d754b9ce7cc3274e2bba2eb
LP5 <noobdoomguy8658@gmail.com>
2021-03-04 16:48:26 +0500

    GitHub Pages - Исправление ошибки в подключении шрифтов

--- dist/sw.js
+++ dist/sw.js
@@ -139,7 +139,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "e74a04049ac910f239e01882246b74fc"
   }, {
     "url": "main.js",
-    "revision": "6983c565d35f3fd4b2fecd7215e2a65c"
+    "revision": "a5d12649fc2d4e88260a49d02eca1c61"
   }], {});
 
 });
