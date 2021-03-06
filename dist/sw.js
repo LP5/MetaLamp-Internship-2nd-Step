0cb2759ca8efd34cca473dae6385f5c905bc5cd6
lp5 <noobdoomguy8658@gmail.com>
2021-03-06 16:07:28 +0500

    Подключен SCSS-миксин для отзыва для большего соответствия макету на Figma

--- dist/sw.js
+++ dist/sw.js
@@ -139,7 +139,7 @@ define("./sw.js",['./workbox-381d62a9'], function (workbox) { 'use strict';
     "revision": "e74a04049ac910f239e01882246b74fc"
   }, {
     "url": "main.js",
-    "revision": "97e154eaea90cce08415999f8c3e2fc8"
+    "revision": "2c6527f8346a68cba660b43f378384ed"
   }], {});
 
 });
