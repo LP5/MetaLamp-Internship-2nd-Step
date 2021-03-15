764bed1338026f95656f3d7db26f646fa3efa6ae
LP5 <noobdoomguy8658@gmail.com>
2021-03-15 16:00:22 +0500

    Доработан функционал Expandable Checkbox List

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,23 +1,17 @@
 window.onload=function() {
-  var checkboxList = document.querySelectorAll("fieldset.checkbox--collapsed");
+  var checkboxList = document.querySelectorAll("#expandable");
   var checkboxListItem = checkboxList.forEach(
     function(e) {
       e.querySelector("legend").addEventListener("click", checkboxExpand);
     }
   );
-  function checkboxExpand() {
-  checkboxList.forEach(
-    function(i) {
-      i.classList.toggle("checkbox--collapsed");
-    }
-  );
-}
+
+  function checkboxExpand(event) {
+    const parent = event.target.parentNode;
+    parent.classList.toggle("checkbox--collapsed");
+  }
 }
 
 function dropdownJS() {
   document.getElementById("dropdownJS").classList.toggle("expanded");
 }
-
-
-
-
