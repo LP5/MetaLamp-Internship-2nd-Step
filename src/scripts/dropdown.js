39e01dba8c5df33d28eeb64f2161c203d6e819d8
LP5 <noobdoomguy8658@gmail.com>
2021-03-17 17:28:35 +0500

    Dropdown получил более грамотный JS, также подчищены его миксины

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,17 +1,26 @@
-window.onload=function() {
-  var checkboxList = document.querySelectorAll("#expandable");
-  var checkboxListItem = checkboxList.forEach(
-    function(e) {
-      e.querySelector("legend").addEventListener("click", checkboxExpand);
-    }
-  );
-
-  function checkboxExpand(event) {
-    const parent = event.target.parentNode;
-    parent.classList.toggle("checkbox--collapsed");
+var checkboxList = document.querySelectorAll("#expandable");
+var checkboxListItem = checkboxList.forEach(
+  function(e) {
+    e.querySelector("legend").addEventListener("click", checkboxExpand);
   }
+);
+
+function checkboxExpand(event) {
+  const parent = event.target.parentNode;
+  parent.classList.toggle("checkbox--collapsed");
 }
 
-function dropdownJS() {
-  document.getElementById("dropdownJS").classList.toggle("expanded");
+var dropdown = document.querySelectorAll(".dropdown");
+dropdown.forEach(
+  function(e) {
+    e.addEventListener("click", expand);
+  }
+);
+
+function expand() {
+  dropdown.forEach(
+    function(e) {
+      e.querySelector(".default").classList.toggle("expanded");
+    }
+  );
 }
